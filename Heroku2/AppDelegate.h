cb09a72c4dfdfce08f80c9f184d6ce7b872d44ca
Sophia09 <lisai121121@gmail.com>
2014-08-21 16:43:35 +0800

    Initial Commit

---
+++ Heroku2/AppDelegate.h
@@ -0,0 +1,15 @@
+//
+//  AppDelegate.h
+//  Heroku2
+//
+//  Created by thinkit  on 8/21/14.
+//  Copyright (c) 2014 thoughtworks. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface AppDelegate : UIResponder <UIApplicationDelegate>
+
+@property (strong, nonatomic) UIWindow *window;
+
+@end
---
+++ Heroku2/main.m
@@ -0,0 +1,18 @@
+//
+//  main.m
+//  Heroku2
+//
+//  Created by thinkit  on 8/21/14.
+//  Copyright (c) 2014 thoughtworks. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+#import "AppDelegate.h"
+
+int main(int argc, char * argv[])
+{
+    @autoreleasepool {
+        return UIApplicationMain(argc, argv, nil, NSStringFromClass([AppDelegate class]));
+    }
+}
